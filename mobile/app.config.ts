10ba7c3f1b35d86f5d4dac5fd88f321b0aeff4ea
minhhd9 <minhhd9@fpt.com>
2026-05-05 13:55:22 +0700

    fix build for mobile

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -43,6 +43,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       "expo-sqlite",
       "expo-web-browser",
       "expo-secure-store",
+      "expo-font"
     ],
     extra: {
       env: env,
